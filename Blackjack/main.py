87c6ead995136680e24602eeb435e6344522790f
AmazonPriime <lukeholland15500@gmail.com>
2020-06-23 19:51:13 +0100

    Tidied up code and more work done for the Game class with updating outputs

--- Blackjack/main.py
+++ Blackjack/main.py
@@ -1,4 +1,4 @@
-import os, random
+import os, sys, random
 
 config = {
     'suits' : {
@@ -12,6 +12,13 @@ config = {
         'king' : 10,
         'queen' : 10,
         'jack' : 10
+    }
+}
+
+commands = {
+    'clear' : {
+        'win32' : 'cls',
+        'cygwin' : 'cls'
     }
 }
 
@@ -75,7 +82,7 @@ class Deck(CardCollection):
         for suit in config['suits']:
             for special in config['special']:
                 self.add(Card(suit, special))
-            for value in range(1, 11):
+            for value in range(2, 11):
                 self.add(Card(suit, value))
 
     def draw(self):
@@ -112,10 +119,14 @@ class Player:
     def __init__(self):
         self.hand = Hand()
         self.bust = False
+        self.stand = False
 
     def hand_check(self):
         self.bust = True if player.hand.hand_value() > 21 else False
         return player.hand.hand_value()
+
+    def set_stand(self):
+        self.stand = True
 
 '''
 Class for Dealer object
@@ -138,11 +149,41 @@ class Game:
         self.player = Player()
         self.dealer = Dealer()
         self.round = 1
+        self.output = ( f"{' '.join(config['suits'].values())} Blackjack - %%round%%\n"
+                        'Current hand: %%player_hand%%\n'
+                        'Dealers hand: x%%dealer_hand_size%% cards\n'
+                        'Cards in deck: x%%deck_size%%\n')
 
     def setup(self):
         self.deck.shuffle()
         self.dealer.deal(self.deck, self.player, 2)
         self.dealer.deal(self.deck, self.dealer, 2)
 
+    def update_output(self):
+        context = {
+            '%%round%%' : self.round,
+            '%%player_hand%%' : self.player.hand,
+            '%%dealer_hand_size%%' : self.dealer.hand.size(),
+            '%%deck_size%%' : self.deck.size()
+        }
+        for tag, value in context.items():
+            self.output = self.output.replace(tag, str(value))
+
+    def display(self):
+        os.system(commands['clear'].get(sys.platform, 'clear'))
+        self.update_output()
+        print(self.output)
+
     def loop(self):
-        pass
+        # main game loop
+        while True:
+            self.display()
+
+
+def main():
+    game = Game()
+    game.setup()
+    game.loop()
+
+if __name__ == '__main__':
+    main()
